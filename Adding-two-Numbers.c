dc01a26e4a4430fe59cdc6a8568f34df95ba2984
manojkeshav <63732667+manojkeshav@users.noreply.github.com>
2020-04-15 20:49:23 +0530

    Update Adding-two-Numbers.c

--- Adding-two-Numbers.c
+++ Adding-two-Numbers.c
@@ -1,2 +1,12 @@
-#<>
-####
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+clrscr();
+printf("enter the value of a and b");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum=%d",sum);
+getch();
+}
